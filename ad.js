e0cab4cb1903e93f9b43e8d71715b0916a7cc794
KatieLeww <klew@student.sdccd.edu>
2023-07-21 11:43:47 -0700

    Substring method practice

--- ad.js
+++ ad.js
@@ -1,15 +1,25 @@
-const heightOutput = document.querySelector("#height");
-const widthOutput = document.querySelector("#width");
+window.addEventListener('DOMContentLoaded', () => {
+  const heightOutput = document.querySelector("#height");
+  const widthOutput = document.querySelector("#width");
 
-function updateSize() {
-  heightOutput.textContent = window.innerHeight;
-  widthOutput.textContent = window.innerWidth;
-}
+  function updateSize() {
+    heightOutput.textContent = window.innerHeight;
+    widthOutput.textContent = window.innerWidth;
+  }
 
-updateSize();
+ updateSize();
 window.addEventListener("size", updateSize);
+
 
 function changeColor(newColor){
   const elem = document.getElementById("color");
   elem.style.color = newColor;
 }
+
+const textLen = document.getElementById('text').textContent.length
+document.getElementById('len').textContent = textLen
+
+let text = document.getElementById("hello").textContent.substring(0,5)
+document.getElementById('world').textContent = text
+
+});
